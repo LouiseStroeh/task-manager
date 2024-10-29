60f237b6c13ac2aa90248bd7a8a45186ecb9724c
LouiseStroehJp <louise.stroeh@jp.dk>
2024-10-29 13:30:03 +0100

    Setup tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'task-manager';
