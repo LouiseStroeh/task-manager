a830d841cd075727b9cb7190ed08e45e6fca770b
LouiseStroehJp <louise.stroeh@jp.dk>
2024-11-02 13:47:16 +0100

    Add subheading component

---
+++ src/app/subheading/subheading.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subheading',
+  standalone: true,
+  imports: [],
+  templateUrl: './subheading.component.html',
+})
+export class SubheadingComponent {}
